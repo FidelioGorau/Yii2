14b104e8def4f7b68385ae8c1646bd9ed8b48f72
Dmitriy Gudz <Dimitriogudz@gmail.com>
2014-10-18 19:00:12 +0300

    Create module for Rest, and create Schools CRUD

---
+++ modules/rest/controllers/SchoolController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\modules\rest\controllers;
+
+use yii\rest\ActiveController;
+
+class SchoolController extends ActiveController
+{
+    public $modelClass = 'app\models\Schools';
+}
